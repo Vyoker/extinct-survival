14f5258f397d49a3bddf075e6acee68173020c2f
Vyoker <mochamadalfi21@gmail.com>
2026-09-19 19:15:27 +0700

    Remove ambiguous characters comment from order code
    Removed comment about avoiding ambiguous characters in order code generation.

--- netlify/functions/utils/order-code.js
+++ netlify/functions/utils/order-code.js
@@ -1,6 +1,5 @@
 const crypto = require('crypto');
 
-// Tanpa 0/O/1/I biar tidak ambigu saat dibaca/diketik ulang manual.
 const ALPHABET = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
 
 function generateOrderCode(length = 6) {
